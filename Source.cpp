bf4ea0c70599d2c766994074c122a6b85d9f10cf
PCandCG <37952236+PCandCG@users.noreply.github.com>
2018-06-24 11:02:56 +0100

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <ctime>
+#include <thread>
+#include <chrono>
+using namespace std;
+
+int main() {
+	int randomNumber;
+	for (int counter = 0; counter < 10; counter = counter + 1) {
+		srand(time(NULL));
+		randomNumber = rand() % 50;
+		cout << randomNumber << endl;
+		this_thread::sleep_for(chrono::seconds(2));
+	}
+	return 0;
+}
